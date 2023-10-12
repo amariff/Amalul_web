79ac0ea493a11c8cf958ac090636615be42c02d0
amariff <amalulr31@gmail.com>
2023-10-12 23:53:02 +0700

    commmit

---
+++ application/views/v_about.php
@@ -0,0 +1,4 @@
+<section>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quae delectus aut eveniet quo, obcaecati eos natus voluptas odit dolore aliquid similique voluptatem ratione ipsam et. Fugit assumenda incidunt consequatur tempore aperiam. Maxime quas ab labore sequi dicta minus, dolore hic rem assumenda enim quasi ipsa doloribus fugiat recusandae voluptatum.</p>
+</section>
---
+++ application/views/v_index.php
@@ -0,0 +1,11 @@
+        <section>
+            <h1> <?php echo $judul ?></h1>
+
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi iure nobis accusamus ullam tempora! Tempora cumque maxime quisquam, facere, temporibus accusamus ipsa laboriosam voluptatibus, asperiores voluptatem doloremque placeat. Nisi eligendi aperiam odio? Tempore voluptate voluptatum est tenetur distinctio adipisci cumque odio sequi pariatur nam, beatae eos quae veniam quo voluptatem.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eum temporibus! Quis quia suscipit ipsa quaerat molestiae, repellat adipisci ut soluta. Accusantium esse ab, rem nostrum ipsam ut veniam voluptas! 
+            </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, laboriosam? Vero voluptatum qui nisi, quod quos reprehenderit minima ipsam velit quaerat cum itaque voluptatibus vitae amet fuga numquam earum id ea voluptates. Sint tempora optio atque ipsum, delectus amet itaque nemo, consectetur ratione ab nobis quo inventore quidem mollitia quia.</p>
+        </section>
